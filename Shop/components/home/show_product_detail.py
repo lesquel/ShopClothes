d10478f4680c542b773e8a3e5e83441acbf7b21f
lesquel <lesquel1319@gmail.com>
2024-10-07 17:09:38 -0500

    HOLAAAAAAAAAAA

--- Shop/components/home/show_product_detail.py
+++ Shop/components/home/show_product_detail.py
@@ -1,52 +1,101 @@
 import flet as ft
+from backend.model.shopping.shoppingCart import ShoppingCart
+from backend.controler.products import PRODUCTS
 def add_to_cart(e):
-    print("Agregar al carrito")
+    ShoppingCart.add_product(PRODUCTS[e.control.selected_index])
+
+def Description(text):
+    return ft.Container(
+        content=ft.Text(text),
+        padding=ft.padding.all(10),
+        alignment=ft.alignment.center,
+        border=ft.border.only(top=ft.border.BorderSide(1, "white")),
+        width=200,
+    )
 
 def show_product_detail(page: ft.Page, product):
     # Importar HomePage aquí para evitar el ciclo de dependencias
     from Shop.pages.home import HomePage
 
-    # Limpiar la página antes de mostrar el detalle
-    page.controls.clear()
-    
+    # Configurar el color de fondo de la página
+    page.padding = ft.padding.all(20)
+
+    # Función para volver a la página principal
+    def go_back(e):
+        page.go('/')
+
     # Crear la vista detallada del producto con mejoras en el diseño
-    detail_view = ft.Container(
+    detail_view = ft.SafeArea(
         content=ft.Column(
             [
-                ft.Text(f"Detalle del Producto", size=35, weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER),
+                ft.Row(
+                    [
+                        ft.IconButton(
+                            icon=ft.icons.ARROW_BACK,
+                            on_click=go_back,
+                        ),
+                        ft.Text(
+                            "Detalle del Producto",
+                            size=35,
+                            weight=ft.FontWeight.BOLD,
+                        ),
+                    ],
+                    alignment=ft.MainAxisAlignment.START,
+                ),
                 ft.Container(
-                    content=ft.Image(src=product.img, width=400, height=400, fit=ft.ImageFit.CONTAIN),
+                    content=ft.Image(
+                        src=product.img,
+                        width=400,
+                        height=400,
+                        fit=ft.ImageFit.CONTAIN,
+                    ),
                     padding=ft.padding.all(20),
                     alignment=ft.alignment.center,
                 ),
-                ft.Text(f"Nombre: {product.name}", size=25, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Descripción: {product.description}", size=18, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Categoría: {product.category.name}", size=18, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Stocks: {product.stock}", size=18, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Tamaño: {product.size.name}", size=18, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Marca: {product.brand.name}", size=18, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Color: {product.color.name}", size=18, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Precio: {product.price}$", size=22, weight=ft.FontWeight.BOLD, color=ft.colors.GREEN, text_align=ft.TextAlign.CENTER),
-                ft.Text(f"Descuento: {product.discount * 100}%", size=18, color=ft.colors.RED, text_align=ft.TextAlign.CENTER),
-                ft.TextButton("Agregar al carrito", icon=ft.icons.ADD_SHOPPING_CART, on_click=add_to_cart),
+                ft.Column(
+                    controls=[
+                        ft.Text(
+                            f"Nombre: {product.name}",
+                            size=25,
+                        ),
+                        ft.Text(
+                            f"Descripción: {product.description}",
+                            size=18,
+                        ),
+                        ft.Row(
+                            controls=[
+                                Description(f"Precio: {product.price}$"),
+                                Description(f"Descuento: {product.discount}%"),
+                                Description(f"Cantidad: {product.stock}"),
+                                Description(f"Tamaño: {product.size.name}"),
+                                Description(f"Color: {product.color.name}"),
+                                Description(f"Marca: {product.brand.name}"),
+                                Description(f"Categoría: {product.category.name}"),
+                            ],
+                            wrap=True,  
+                        ),
+                        ft.ElevatedButton(
+                            "Agregar al carrito",
+                            icon=ft.icons.ADD_SHOPPING_CART,
+                            on_click=add_to_cart,
+                            
+                        ),
+                    ],
+                    expand=True,
+                    alignment=ft.alignment.center,
+                    horizontal_alignment=ft.alignment.center,
+                    spacing=20,
+                ),
             ],
-            alignment=ft.MainAxisAlignment.START,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            spacing=15,
+            expand=True,
             scroll=ft.ScrollMode.AUTO,
+            horizontal_alignment=ft.alignment.center,
         ),
-        width=page.width,
-        height=page.height,
-        padding=ft.padding.only(top=20, bottom=100, left=20, right=20),
-        alignment=ft.alignment.top_center,
-        border_radius=ft.border_radius.all(20),
-        shadow=ft.BoxShadow(
-            blur_radius=10,
-            spread_radius=5,
-            color=ft.colors.BLACK12,
-        )
+        expand=True,
     )
-    
+
+    # Limpiar la página antes de mostrar el detalle
+    page.controls.clear()
     # Añadir la vista detallada a la página
     page.add(detail_view)
     page.update()
